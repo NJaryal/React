fed21f975a81a6add0fda328efc1fc51f056c029
Nitin Jaryal <Nitin_Jaryal@epam.com>
2019-07-24 21:45:40 +0530

    Cypress test cases

--- cypress/integration/elementChecks.spec.js
+++ cypress/integration/elementChecks.spec.js
@@ -5,8 +5,19 @@ describe('Integartion', () => {
     });
 
     it('blank input field', () => {
+      const userTypedMsg = 'Tomb Raider';
       cy.get('[data-test="SearchComponent"]')
-        .should('have.length', 0);
+        .should('have.value', userTypedMsg);
+    });
+
+    it('blank input field', () => {
+      cy.get('[data-test="activeSearch"]')
+        .contains('Title');
+    });
+
+    it('blank input field', () => {
+      cy.get('[data-test="activeSort"]')
+        .contains('RELEASE DATE');
     });
   });
 
