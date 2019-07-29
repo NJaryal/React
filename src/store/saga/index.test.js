620e3a345f6d4d3e8d2f60f5bf5ef81ad0ab0a9a
Nitin Jaryal <Nitin_Jaryal@epam.com>
2019-07-29 23:18:03 +0530

    Saga sample code

--- src/store/saga/index.test.js
+++ src/store/saga/index.test.js
@@ -2,20 +2,21 @@ import sinon from 'sinon';
 import { moviesData as api } from '../../api/index';
 export { fetchProducts } from './index';
 
-test('callApi', async (assert) => {
-  const dispatched = [];
-  sinon.stub(api, 'myApi').callsFake(() => ({
-    json: () => ({
-      some: 'value',
-    }),
-  }));
-  const url = 'https://reactjs-cdp.herokuapp.com/movies/';
-  const result = await runSaga({
-    dispatch: (action) => dispatched.push(action),
-    getState: () => ({ state: 'test' }),
-  }, fetchProducts, url).toPromise();
+describe('Saga Api', () => {
+  it('Call generators', async (assert) => {
+    const dispatched = [];
+    sinon.stub(api, 'sagaApi').callsFake(() => ({
+      json: () => ({
+        data: 'value',
+      }),
+    }));
+    const url = 'https://reactjs-cdp.herokuapp.com/movies/';
+    const result = await runSaga({
+      dispatch: (action) => dispatched.push(action),
+      getState: () => ({ state: 'test' }),
+    }, fetchProducts, url).toPromise();
 
-  assert.true(api.calledWith(url, somethingFromState({ state: 'test' })));
-  assert.deepEqual(dispatched, [moviesData({ some: 'value' })]);
+    assert.true(api.calledWith(url, somethingFromState({ state: 'test' })));
+    assert.deepEqual(dispatched, [moviesData({ some: 'value' })]);
+  });
 });
-
